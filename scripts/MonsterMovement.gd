a113c484a97d4816aa07a1076f1facb6ca84e9cd
KIO-LAPTOP\oskar <oskarvengengel@gmail.com>
2024-08-11 16:58:20 +0200

    Monster State machine setup
    preparing to split work of monster behaviour. (and disabled player jump again)

--- scripts/MonsterMovement.gd
+++ scripts/MonsterMovement.gd
@@ -4,9 +4,35 @@ extends RigidBody2D
 @export var maxSpeed = 2
 @onready var player = $"../Player"
 
-func _integrate_forces(state):
+
+var states = ['Bushcamp', 'Follow', 'Search']
+var currentState = 1
+
+func _process(delta):
+	if (currentState == 0):
+		Bushcamp(delta)
+	if (currentState == 1):
+		Follow(delta)
+	if (currentState == 2):
+		Search(delta)
+
+func Bushcamp(delta: float):
+	pass
+	
+func Follow(delta: float):
 	var monsterPosition = position
 	var playerPosition =  player.position
 	var movementDirection = playerPosition - monsterPosition
 	var movementVector = movementDirection.normalized()
 	linear_velocity = Vector2(clamp(movementVector.x * speed, -maxSpeed, maxSpeed), linear_velocity.y)
+
+func Search(delta: float):
+	pass
+
+
+
+func setState(state : int):
+	currentState = clamp(state, 0, states.count - 1)
+
+func getState(intState = currentState):
+	return states[intState]
--- scripts/player.gd
+++ scripts/player.gd
@@ -46,7 +46,7 @@ func _physics_process(delta):
 		hide_interact()
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jumpable and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
